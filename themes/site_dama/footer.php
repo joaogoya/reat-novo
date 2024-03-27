65305abe0d20a00f31e71cc2680a3efb7d354e03
Joao <joaogoya@gmail.com>
2024-03-27 17:12:22 -0300

    erros diferentes ajuste

--- themes/site_dama/footer.php
+++ themes/site_dama/footer.php
@@ -1,5 +1,5 @@
             <footer class="footer-border-top">
-                <section>
+                <section> dd
                     <div class="whatsapp">
                         <?php get_template_part('includes/template-parts/footer/whatsapp'); ?>
                     </div>
